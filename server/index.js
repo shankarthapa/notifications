355a0edc0bab1e67670cade15f0dd4ecea2f3b11
Shankar Thapa <shankasthapa@gmail.com>
2018-10-02 17:48:28 +0530

    updated node server code

--- server/index.js
+++ server/index.js
@@ -5,27 +5,25 @@ var http = require('http'),
 
 // Send index.html to all requests
 var app = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end(index);
 });
 
 // Socket.io server listens to our app
 var io = require('socket.io').listen(app);
 
-// Send current time to all connected clients
-function sendTime() {
-    io.emit('time', { time: new Date().toJSON() });
-}
-
-// Send current time every 10 secs
-setInterval(sendTime, 10000);
-
 // Emit welcome message on connection
 io.on('connection', function(socket) {
     // Use socket to communicate with this particular client only, sending it it's own id
     socket.emit('welcome', { message: 'Welcome!', id: socket.id });
 
-    socket.on('i am client', console.log);
+    // takes push msg from admin & sends to all connected clients
+    socket.on('push', function(msg) {
+        // send notifications to all connected clients
+        io.emit('notifications', { msg: msg });
+    });
 });
+
+console.log('Node With Socket.io started at port: 8000');
 
 app.listen(8000);
